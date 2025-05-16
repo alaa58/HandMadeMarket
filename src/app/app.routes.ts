1534d6f871f07088ae60db11f769314bf023be6c
alaa ragab <alaragab19@gmail.com>
2025-05-16 18:02:26 +0300

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,26 @@ import { Routes } from '@angular/router';
 import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
 import { BlankLayoutComponent } from './layout/blank-layout/blank-layout.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { CartComponent } from './components/cart/cart.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductComponent } from './components/product/product.component';
 
 export const routes: Routes = [
-    {path:'auth', component: AuthLayoutComponent},
-    {path: 'blank', component: BlankLayoutComponent},
+    {path:'', component: AuthLayoutComponent, children:[
+        {path:'', redirectTo:'login', pathMatch:'full'},
+        {path: 'login', component:LoginComponent},
+        {path: 'register', component: RegisterComponent}
+    ]},
+    {path: '', component: BlankLayoutComponent, children: [
+        {path:'', redirectTo:'home', pathMatch:'full'},
+        {path: 'cart', component: CartComponent},
+        {path: 'categories', component: CategoriesComponent},
+        {path: 'home', component:HomeComponent},
+        {path:'product', component:ProductComponent}
+    ]},
     {path: '**', component: NotfoundComponent}
 ];
--- src/app/layout/blank-layout/blank-layout.component.ts
+++ src/app/layout/blank-layout/blank-layout.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { NavBlankComponent } from "../../components/nav-blank/nav-blank.component";
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-blank-layout',
-  imports: [],
+  imports: [NavBlankComponent, RouterOutlet, FooterComponent],
   templateUrl: './blank-layout.component.html',
   styleUrl: './blank-layout.component.css'
 })
