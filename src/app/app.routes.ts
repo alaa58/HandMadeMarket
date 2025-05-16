b9a6a5b5af648aa566b169914187e917be7a47eb
alaa ragab <alaragab19@gmail.com>
2025-05-16 17:36:33 +0300

    addfiles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
+import { BlankLayoutComponent } from './layout/blank-layout/blank-layout.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'auth', component: AuthLayoutComponent},
+    {path: 'blank', component: BlankLayoutComponent},
+    {path: '**', component: NotfoundComponent}
+];
