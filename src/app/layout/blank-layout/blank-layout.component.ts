05bd2d731c2cba867e803e36715bdacff8e23d84
madonna-hany-boils <mh2153@fayoum.edu.eg>
2025-05-17 02:01:35 +0300

    first commit

---
+++ src/app/layout/blank-layout/blank-layout.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavBlankComponent } from "../../components/nav-blank/nav-blank.component";
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "../../components/footer/footer.component";
+
+@Component({
+  selector: 'app-blank-layout',
+  imports: [NavBlankComponent, RouterOutlet, FooterComponent],
+  templateUrl: './blank-layout.component.html',
+  styleUrl: './blank-layout.component.css'
+})
+export class BlankLayoutComponent {
+
+}
