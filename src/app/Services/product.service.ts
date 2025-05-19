0ba03fbd74abaa41acecb51c153352c23c0337a8
Your Name <you@example.com>
2025-05-20 00:57:23 +0200

    add Productdetails

--- src/app/Services/product.service.ts
+++ src/app/Services/product.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IProduct } from '../models/product.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,8 @@ import { IProduct } from '../models/product.model';
 export class ProductService {
 productList:any[]=[];
   constructor( private http:HttpClient) { }
-baseUrl="'https://handmade.runasp.net/api/Product";
-//https://fakestoreapi.com/products";
+baseUrl=" https://fakestoreapi.com/products";
+bsaeUrl2 ='https://handmade.runasp.net/api';
 
 getAllProducts(pageNumber: number, pageSize: number): Observable<any[]> {
    return this.http.get<any[]>(this.baseUrl, {
@@ -32,4 +33,11 @@ getById(productId:number):Observable<any>{
     );
 }
 //all any will replace by Iproduct
+
+
+//get product By ID
+getProductById(id:string |null):Observable<any>
+{
+  return this.http.get(`https://handmade.runasp.net/api/Product/${id}`)
+}
 }
--- src/app/components/categories/categories.component.ts
+++ src/app/components/categories/categories.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-category',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule,RouterLink],
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.css']
 })
